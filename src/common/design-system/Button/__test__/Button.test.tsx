ce773555a4cdd22ea0928eb84c14673ccecfa349
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2026-03-17 15:13:11 +0100

    test(button): add onClick handler test

--- src/common/design-system/Button/__test__/Button.test.tsx
+++ src/common/design-system/Button/__test__/Button.test.tsx
@@ -1,5 +1,6 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import  Button from '../Button'; 
 
 describe('Button component', () => {
@@ -7,5 +8,12 @@ describe('Button component', () => {
         render(<Button onClick={() => {}}>Click Me</Button>);
         const buttonElement = screen.getByText('Click Me');
         expect(buttonElement).toBeInTheDocument();
+    })
+    it('calls the onClick handler when clicked', async () => {
+        const handleClick = vi.fn();
+        render(<Button onClick={handleClick}>Click Me</Button>);
+        const buttonElement = screen.getByText('Click Me');
+        await userEvent.click(buttonElement);
+        expect(handleClick).toHaveBeenCalledTimes(1);
     });
 });
